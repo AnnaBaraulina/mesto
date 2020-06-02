4a64d5b2c6000e3a402c6bedf2261f2bd6723982
AnnaBaraulina <a.baraulina@yandex.ru>
2020-06-02 17:59:58 +0300

    review 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,30 @@
-let popup = document.querySelector(".popup");
+const popup = document.querySelector(".popup");
 const popupOpen = document.querySelector(".profile__edit-button");
 const popupClose = document.querySelector(".popup__close-icon");
 
 let infoName = document.querySelector(".profile__name");
 let infoAbout = document.querySelector(".profile__about");
-let containerName = document.querySelector(".popup__name");
-let containerAbout = document.querySelector(".popup__about");
+let containerName = document.querySelector("#name");
+let containerAbout = document.querySelector("#career");
 
 const popupSubmitButton = document.querySelector(".popup__submit");
 
-popupOpen.onclick = function () {
-  popup.style.display="flex";
+function openPopup() {
+  popup.classList.add('popup_opened');
+  console.log('popup явись');
+
   containerName.value = infoName.innerText;
   containerAbout.value = infoAbout.innerText;
-};
+}
 
-popupClose.onclick = function () {
-  popup.style.display="none";
-};
+function closePopup() {
+  popup.classList.remove('popup_opened');
+  console.log('popup закройся');
+}
+
+
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 
 // Находим форму в DOM
 let formElement = document.querySelector(".popup__container");
@@ -26,12 +33,9 @@ let formElement = document.querySelector(".popup__container");
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   
-  // Находим поля формы в DOM
+ 
   
-  let containerName = document.querySelector(".popup__name");
-  let containerAbout = document.querySelector(".popup__about");
-  
-  // Получите значение полей из свойства value
+ 
  
 
   // Выберите элементы, куда должны быть вставлены значения полей
@@ -40,6 +44,7 @@ function formSubmitHandler (evt) {
 
   infoName.textContent = containerName.value;
   infoAbout.textContent = containerAbout.value;
+  popupClose();
   
 }
 
@@ -47,8 +52,9 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 
+popupSubmitButton.addEventListener('click', closePopup);
 
-popupSubmitButton.addEventListener('click', popupClose.onclick);
+
 
 
 
